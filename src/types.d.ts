af02740e3410c74093a1f0e4848272861ca4cf57
DavidEP <56250584+davidmoltin@users.noreply.github.com>
2020-12-02 17:06:40 +0000

    Update types.d.ts with new definitions

--- src/types.d.ts
+++ src/types.d.ts
@@ -5,3 +5,11 @@ declare module 'sharethis-reactjs' {
 declare module 'algolia-places-react'
 
 declare module 'react-stripe-elements'
+
+declare module 'aos/dist/aos'
+
+declare module 'material-ui-cookie-consent'
+
+declare module 'react-animated-slider'
+
+declare module 'react-swipeable-views'
