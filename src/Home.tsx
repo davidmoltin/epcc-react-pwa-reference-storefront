bb04291f96bfb948e13a063e5205cf6ac6845244
DavidEP <56250584+davidmoltin@users.noreply.github.com>
2020-12-03 12:19:24 +0000

    Remove B2c landing page

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { config } from './config';
-import { HomeB2c } from './HomeB2c';
 import { HomeB2b } from './HomeB2b';
 import { HomeSlide } from './HomeSlide';
 
