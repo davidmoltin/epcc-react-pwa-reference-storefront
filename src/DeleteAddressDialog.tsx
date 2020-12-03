db958d7968b823d19714f706c2321c88a76d31cd
DavidEP <56250584+davidmoltin@users.noreply.github.com>
2020-12-03 07:38:30 +0000

    Update with Material UI style

--- src/DeleteAddressDialog.tsx
+++ src/DeleteAddressDialog.tsx
@@ -3,6 +3,46 @@ import Modal from 'react-responsive-modal';
 import { ReactComponent as CloseIcon } from './images/icons/ic_close.svg';
 import { useTranslation } from './app-state';
 import './DeleteAddressDialog.scss';
+import {
+  Button,
+  IconButton
+} from '@material-ui/core';
+import {
+  createStyles,
+  makeStyles,
+  Theme,
+  withStyles,
+} from '@material-ui/core/styles';
+import { red } from '@material-ui/core/colors';
+import {
+  CancelOutlined,
+  DeleteOutlined
+} from '@material-ui/icons';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    margin: {
+      margin: theme.spacing(1),
+    },
+      button: {
+        margin: theme.spacing(1),
+      },
+    buttonWarning: {
+      margin: theme.spacing(1),
+      backgroundcolor: theme.palette.warning.main,
+    },
+  }),
+);
+
+const ColorButton = withStyles((theme: Theme) => ({
+  root: {
+    color: theme.palette.getContrastText(red[500]),
+    backgroundColor: red[500],
+    '&:hover': {
+      backgroundColor: red[700],
+    },
+  },
+}))(Button);
 
 interface DeleteAddressDialogParams {
   handleCancelDelete: (...args: any[]) => any,
@@ -15,6 +55,7 @@ export const DeleteAddressDialog: React.FC<DeleteAddressDialogParams> = (props) 
   const { handleCancelDelete, onDeleteAddress, isDeleteModalOpen, isLoading } = props;
 
   const { t } = useTranslation();
+  const classes = useStyles();
 
   return (
   <Modal open={isDeleteModalOpen} onClose={handleCancelDelete} classNames={{modal: 'addressdelete'}} showCloseIcon={false}>
@@ -26,20 +67,30 @@ export const DeleteAddressDialog: React.FC<DeleteAddressDialogParams> = (props) 
         <h2 className="addressdelete__title">
           {t('delete-address')}
         </h2>
-        <button type="button" aria-label="close" onClick={handleCancelDelete}>
-          <CloseIcon/>
-        </button>
+        <IconButton aria-label="close" onClick={handleCancelDelete} className={classes.margin}>
+          <CancelOutlined fontSize="inherit" />
+        </IconButton>
       </div>
       <div className="addressdelete__body">
         {t('delete-address-message')}
       </div>
       <div className="epform__group --btncontainer">
-        <button className="epbtn" type="button" onClick={handleCancelDelete}>
-          {t('cancel')}
-        </button>
-        <button className="epbtn --secondary" type="button" onClick={onDeleteAddress}>
-          {t('delete')}
-        </button>
+      <Button
+            variant="outlined"
+            className={classes.button}
+            onClick={handleCancelDelete}
+            color="primary"
+            startIcon={< CancelOutlined />}
+          > {t('cancel')}
+        </Button>
+        <ColorButton
+            variant="contained"
+            onClick={onDeleteAddress}
+            disableElevation
+            startIcon={< DeleteOutlined />}
+            color="primary"
+          > {t('delete')}
+        </ColorButton>
       </div>
     </div>
   </Modal>
