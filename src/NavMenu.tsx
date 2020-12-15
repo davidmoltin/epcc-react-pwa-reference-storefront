b6a28860fa6ff6facc9c8b58f75ea61fc3da1140
DavidEP <56250584+davidmoltin@users.noreply.github.com>
2020-12-15 12:09:04 +0000

    Update Hierarchy Menu

--- src/NavMenu.tsx
+++ src/NavMenu.tsx
@@ -5,6 +5,7 @@ import * as moltin from '@moltin/sdk';
 import { useCategories } from './app-state';
 
 import './NavMenu.scss';
+import { Button } from '@material-ui/core';
 
 interface NavMenuProps {
   categoryHistory: string[];
@@ -34,7 +35,13 @@ export const NavMenu: React.FC<NavMenuProps> = (props) => {
                 className={`navmenu__link ${category.children ? '--haschildren' : ''}`}
                 to={createCategoryUrl(category.slug)}
               >
-                {category.name}
+                <Button
+                  size="small" 
+                  fullWidth
+                  color="secondary" 
+                >
+                  {category.name}
+                </Button>
               </Link>
               <button type="button" className={`navmenu__nextbutton ${category.children ? '--haschildren' : ''}`} onClick={() => handleShow(category)} />
             {category.children && renderCategories(category.children, level + 1, categoryHistory.includes(category.id))}
